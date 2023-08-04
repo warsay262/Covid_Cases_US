62aadbd192e3f3f21521eacaacce3e59e4d057cd
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2023-08-03 20:49:56 -0400

    Update Covid_Interaction.py

--- Covid_Interaction.py
+++ Covid_Interaction.py
@@ -1,31 +1,38 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load the CSV data
+# Load the data
 data = pd.read_csv('Covid_Cases_2020-2022.csv')
+data['Date'] = pd.to_datetime(data['Date'])
 
-# Create a Streamlit sidebar with state and year selection
-st.sidebar.title("COVID-19 Dashboard")
-selected_state = st.sidebar.selectbox("Select a state", data['US_States'].unique())
-selected_year = st.sidebar.selectbox("Select a year", [2020, 2021, 2022])
+# Streamlit app
+st.title('Interactive COVID-19 Dashboard')
 
-# Filter the data based on the selected state and year
-filtered_data = data[(data['US_States'] == selected_state) & (data['Date'].str.endswith(str(selected_year)))]
+# Sidebar - State selection and year range
+selected_state = st.sidebar.selectbox('Select a State', data['US_States'].unique())
+selected_year_range = st.sidebar.slider('Select Year Range', 2020, 2022, (2020, 2022))
 
-# Calculate the total confirmed cases
-total_confirmed = filtered_data['Confirmed'].sum()
+# Filter data based on selected state and year range
+filtered_data = data[(data['US_States'] == selected_state) & (data['Date'].dt.year >= selected_year_range[0]) & (data['Date'].dt.year <= selected_year_range[1])]
 
-# Display the total confirmed cases
-st.write(f"Total confirmed cases in {selected_state} in {selected_year}: {total_confirmed}")
+# Display table of filtered data
+st.write(f'Selected State: {selected_state}')
+st.write(f'Selected Year Range: {selected_year_range[0]} - {selected_year_range[1]}')
+st.write(filtered_data[['Date', 'Confirmed']].set_index('Date'))
 
-# Create a line chart for confirmed cases over the years
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(filtered_data['Date'], filtered_data['Confirmed'], marker='o')
-ax.set_xticklabels(filtered_data['Date'], rotation=45)
-ax.set_xlabel('Date')
-ax.set_ylabel('Confirmed Cases')
-ax.set_title(f'Confirmed Cases in {selected_state} ({selected_year})')
+# Group data by date and calculate total confirmed cases
+daily_cases = filtered_data.groupby('Date')['Confirmed'].sum()
 
-# Display the Matplotlib plot using Streamlit
-st.pyplot(fig)
+# Create a line chart
+plt.figure(figsize=(10, 6))
+sns.lineplot(x=daily_cases.index, y=daily_cases.values)
+plt.xlabel('Date')
+plt.ylabel('Confirmed Cases')
+plt.title('Confirmed COVID-19 Cases Over Time')
+plt.xticks(rotation=45)
+st.pyplot()
+
+# Add a disclaimer
+st.write("Note: This dashboard is for demonstration purposes only and the data used may not be up-to-date.")
